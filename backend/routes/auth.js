39b1a5cadbf40cca28cb635f52167648a045c5cc
DeepralShakya <shakyadeepral44@gmail.com>
2024-01-18 18:13:12 +0545

    thunderclient collections to manage request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,23 +5,32 @@ const { body, validationResult } = require('express-validator');
 
 //create user using POST "/api/auth/"
 
-router.post('/',[
+router.post('/createuser',[
     body('name', 'lease enter a valid name').isLength({ min: 3 }),
     body('email', 'Please enter a valid email').isEmail(),
   // password must be at least 5 chars long
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-] ,(req, res)=>{
+] ,async (req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    
+    try{
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+      return res.status(400).json({error: "User with this email already exist"})
+    }
+        user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-
-      }).then(user => res.json(user)).catch(err=> {console.log(err)
-        res.json({error: 'Please enter unique email or name value', message: err.message})})
+      })     
+      res.json(user)
+    }catch(error){
+      console.error(error.message)
+      res.status(500).send("Some error occured")
+    }
 })
 
 module.exports = router
