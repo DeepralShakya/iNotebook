2ce689372142721020f1b43986a53297a7d2dd90
DeepralShakya <shakyadeepral44@gmail.com>
2024-01-22 16:38:11 +0545

    creating login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,8 +7,7 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'Deepral';
 
-//create user using POST "/api/auth/"
-
+//create user using POST "/api/auth/createuser"
 router.post('/createuser',[
     body('name', 'lease enter a valid name').isLength({ min: 3 }),
     body('email', 'Please enter a valid email').isEmail(),
@@ -42,7 +41,41 @@ router.post('/createuser',[
   
     }catch(error){
       console.error(error.message)
-      res.status(500).send("Some error occured")
+      res.status(500).send("Internal server error")
+    }
+})
+
+//create user using POST "/api/auth/login"
+router.post('/login',[
+  body('email', 'Please enter a valid email').isEmail(),
+  body('password', 'Password cannot be empty').exists(),
+] ,async (req, res)=>{
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const{email, password} = req.body;
+    try{
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error: "Try to enter correct credentials"});      
+    }
+    const passwordCompared = await bcrypt.compare(password, user.password);
+    if(!passwordCompared){
+      return res.status(400).json({error: "Try to enter correct credentials"});      
+    }
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken}) 
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server error");
     }
 })
 
